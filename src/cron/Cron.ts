80512842d6e5930bbeae7bc25151f50ac55b92cb
Aex <spryti@qq.com>
2020-06-03 01:34:41 +0800

    优化Queue Cron模块

--- src/cron/Cron.ts
+++ src/cron/Cron.ts
@@ -1,32 +1,55 @@
+import { echo } from 'coa-echo'
 import { env } from 'coa-env'
 import { _ } from 'coa-helper'
+import { Queue } from '..'
 import redis from '../redis'
 import { Dic } from '../typings'
 import { CronTime } from './CronTime'
 
+const D = { series: 0 }
 const prefix = env.redis.prefix + '-aac-cron-'
 const key_cron_last = prefix + 'last'
 
 export class Cron {
 
-  protected jobs: Dic<{ time: string, worker: () => any }> = {}
+  private readonly times: Dic<string>
+  private readonly workers: Dic<() => Promise<void>>
+  private readonly queue: { push: (id: string) => Promise<number> }
+
+  constructor (queue: Queue) {
+    this.times = {}
+    this.workers = {}
+    this.queue = queue.job(id => this.work(id), 'AAC_CRON')
+  }
+
+  // 添加日程计划
+  job (time: string, worker: () => Promise<void>) {
+    const id = ++D.series
+    this.times[id] = time
+    this.workers[id] = worker
+  }
 
   // 尝试触发
   async try () {
     const deadline = _.now()
     const start = _.toInteger(await redis.io.getset(key_cron_last, deadline)) || (deadline - 1000)
-    _.forEach(this.jobs, ({ time }, name) => {
+    _.forEach(this.times, (time, id) => {
       const next = new CronTime(time, { start, deadline }).next()
-      if (next) this.onJob(name)
+      if (next) this.queue.push(id)
     })
   }
 
-  // 工作者
-  async worker (name: string) {
-    const { worker } = this.jobs[name] || {}
-    worker && await worker()
+  // 开始执行
+  private async work (id: string) {
+    const worker = this.workers[id]
+    if (worker) {
+      try {
+        await worker()
+      } catch (e) {
+        echo.error('* Cron JobError: %s %s', id, this.times[id], e.toString())
+      }
+    } else {
+      echo.error('* Cron JobNotFound: %s %s', id, this.times[id])
+    }
   }
-
-  // 当有新的任务的时候
-  protected onJob (name: string) {}
 }
--- src/queue/Queue.ts
+++ src/queue/Queue.ts
@@ -2,21 +2,28 @@ import { echo } from 'coa-echo'
 import { env } from 'coa-env'
 import { $, _, } from 'coa-helper'
 import redis from '../redis'
+import { Dic } from '../typings'
 
-const D = { lock: false }
-const prefix = env.redis.prefix + '-{aac-queue}-'
-const key_pending = prefix + 'pending'
-const key_doing = prefix + 'doing'
-const key_doing_map = prefix + 'doing-map'
-const key_retrying = prefix + 'retrying'
+const D = { lock: false, series: 0 }
 const sep = '^^'
-
-const escape = (s: string) => Buffer.from(s, 'utf8').toString('base64').replace(/=/g, '')
-const unescape = (s: string) => Buffer.from(s, 'base64').toString('utf8')
 
 export class Queue {
 
-  doingJobId = ''
+  private doingJob: string
+  private readonly K: { pending: string, doing: string, doing_map: string, retrying: string }
+  private readonly workers: Dic<(id: string, data: string) => Promise<void>>
+
+  constructor (name: string) {
+    const prefix = +`${env.redis.prefix}-{aac-queue-${name}-`
+    this.K = {
+      pending: prefix + 'pending',
+      doing: prefix + 'doing',
+      doing_map: prefix + 'doing-map',
+      retrying: prefix + 'retrying'
+    }
+    this.workers = {}
+    this.doingJob = ''
+  }
 
   // 初始化任务，interval为上报间隔，默认为10秒上报一次
   async init (interval = 10e3) {
@@ -31,83 +38,96 @@ export class Queue {
     // 持续监听队列
     while (1) {
       try {
-        const jobId = await redis_queue.brpoplpush(key_pending, key_doing, 0)
-        await this.onReceive(jobId)
+        const key = await redis_queue.brpoplpush(this.K.pending, this.K.doing, 0)
+        await this.work(key)
       } catch (e) {
-        echo.error(e)
+        echo.error('* QueueError:', e)
         await $.timeout(2000)
       }
     }
   }
 
-  // 推送新任务
-  async push (name: string, id: string, data = '') {
-    const jobId = name + sep + escape(id) + sep + escape(data)
-    echo.grey('* Queue: push new job %s', jobId)
-    return await redis.io.lpush(key_pending, jobId)
+  // 添加新Job类型
+  job (worker: (id: string, data: string) => Promise<void>, name = `Job${++D.series}`) {
+    this.workers[name] = worker
+    const push = (id: string, data = '') => this.push(name, id, data)
+    return { push }
   }
 
   // 检查队列，force是否强制执行，timeout任务上报超时的时间，默认60秒，interval两次执行间隔，默认60秒
   async retry (force = false, timeout = 60e3, interval = 60e3) {
     const now = _.now()
     // 如果60秒内执行过且没有强制执行，则忽略
-    const can = await redis.io.set(key_retrying, now, 'PX', interval, 'NX')
+    const can = await redis.io.set(this.K.retrying, now, 'PX', interval, 'NX')
     if (!can && !force) return
 
-    const [[, doing = []], [, doing_map = {}]] = await redis.io.pipeline().lrange(key_doing, 0, -1).hgetall(key_doing_map).exec()
-    const retryJobIds = [] as string[]
+    const [[, doing = []], [, doing_map = {}]] = await redis.io.pipeline().lrange(this.K.doing, 0, -1).hgetall(this.K.doing_map).exec()
+    const retryJobs = [] as string[]
 
     // 遍历map检查是否超时
     _.forEach(doing_map, (time, jobId) => {
       doing_map[jobId] = now - _.toInteger(time)
-      if (doing_map[jobId] >= timeout) retryJobIds.push(jobId)
+      if (doing_map[jobId] >= timeout) retryJobs.push(jobId)
     })
     // 遍历doing检查是否超时
     _.forEach(doing, jobId => {
-      if (!doing_map[jobId] || doing_map[jobId] >= timeout) retryJobIds.push(jobId)
+      if (!doing_map[jobId] || doing_map[jobId] >= timeout) retryJobs.push(jobId)
     })
 
     // 如果存在需要重试的任务
-    if (retryJobIds.length) {
-      const uniqJobIds = _.uniq(retryJobIds)
-      await redis.io.pipeline().hdel(key_doing_map, ...uniqJobIds).lpush(key_pending, ...uniqJobIds).exec()
+    if (retryJobs.length) {
+      const uniqJobIds = _.uniq(retryJobs)
+      await redis.io.pipeline().hdel(this.K.doing_map, ...uniqJobIds).lpush(this.K.pending, ...uniqJobIds).exec()
     }
   }
 
-  // 任务具体工作者
-  protected async worker (name: string, id: string, data: string) {
-
+  // 推送新任务
+  private async push (name: string, id: string, data = '') {
+    const jobId = name + sep + escape(id) + sep + escape(data)
+    echo.grey('* Queue: push new job %s', jobId)
+    return await redis.io.lpush(this.K.pending, jobId)
   }
 
-  // 当收到新任务的时候
-  private async onReceive (jobId: string) {
+  // 开始工作
+  private async work (job: string) {
 
     // 准备执行
     const now = _.now()
-    const can = await redis.io.hsetnx(key_doing_map, jobId, now)
+    const can = await redis.io.hsetnx(this.K.doing_map, job, now)
 
     // 如果已经开始，则忽略
     if (!can) return
 
     // 开始执行
-    this.doingJobId = jobId
-    echo.grey('* Queue: start job %s', jobId)
-    const [name, id, data] = jobId.split(sep)
-    await this.worker(name, unescape(id), unescape(data)).catch(e => {
-      echo.error('* Queue JobError: %s %s', jobId, e.toString())
-    })
+    this.doingJob = job
+    echo.grey('* Queue: start job %s', job)
+
+    // 解析Job
+    const [name, id, data] = job.split(sep)
+    const worker = this.workers[name]
+
+    // 执行Job
+    if (worker) {
+      try {
+        await worker(id, data)
+      } catch (e) {
+        echo.error('* Queue JobError: %s %s', job, e.toString())
+      }
+    } else {
+      echo.error('* Queue JobNotFound: %s %s %s', name, id, data)
+    }
 
     // 执行结束
-    this.doingJobId = ''
-    await redis.io.pipeline().hdel(key_doing_map, jobId).lrem(key_doing, 0, jobId).exec()
-    echo.grey('* Queue: job %s completed in %sms', jobId, _.now() - now)
+    this.doingJob = ''
+    await redis.io.pipeline().hdel(this.K.doing_map, job).lrem(this.K.doing, 0, job).exec()
+    echo.grey('* Queue: job %s completed in %sms', job, _.now() - now)
   }
 
   // 定时检查
   private async interval () {
     // 如果当前有正在执行的任务，报告最新时间
-    if (this.doingJobId)
-      await redis.io.hset(key_doing_map, this.doingJobId, _.now())
+    if (this.doingJob)
+      await redis.io.hset(this.K.doing_map, this.doingJob, _.now())
   }
 
 }
